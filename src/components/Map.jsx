d3bc069e9359d0f1850f9733003e20c48c2773c6
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-05-23 21:33:35 +0330

    feat: change the some settings from map Leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -14,12 +14,12 @@ function Map() {
         <MapContainer
           center={mapPosition}
           zoom={13}
-          scrollWheelZoom={false}
+          scrollWheelZoom={true}
           className={styles.map}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
           />
           <Marker position={mapPosition}>
             <Popup>
