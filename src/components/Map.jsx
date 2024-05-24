7b97ab606730011e1e82d50555f4f367a7327af3
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-05-24 13:05:37 +0330

    feat: add map loop for Marker component for all cities

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,34 +1,41 @@
 // import { useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState } from "react";
+// import { useState } from "react";
+import { useCities } from "../contexts/CitiesContext";
 function Map() {
   // const [searchParams, setSearchParams] = useSearchParams();
   // const lat = searchParams.get("lat");
   // const lng = searchParams.get("lng");
   // const navigate = useNavigate();
-  const [mapPosition, setMapPosition] = useState([42, 0]);
-  if (setMapPosition)
-    return (
-      <div className={styles.mapContainer}>
-        <MapContainer
-          center={mapPosition}
-          zoom={13}
-          scrollWheelZoom={true}
-          className={styles.map}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-          />
-          <Marker position={mapPosition}>
+  // const [mapPosition, setMapPosition] = useState([42, 0]);
+  // if (setMapPosition)
+  const { cities } = useCities();
+  return (
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={[20, 0]}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.div}
+          >
             <Popup>
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
           </Marker>
-        </MapContainer>
-      </div>
-    );
+        ))}
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
