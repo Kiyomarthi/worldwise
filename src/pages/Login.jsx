ce4137ba729561f48954b86528a9bbc85d942442
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-05-28 13:36:48 +0330

    feat: navigate to the app page if the email and password is currect by useEffect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,28 @@
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "../components/Button";
 import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
-  const { login } = useAuth();
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
     if (email && password) login(email, password);
   }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app");
+    },
+    [isAuthenticated]
+  );
 
   return (
     <main className={styles.login}>
