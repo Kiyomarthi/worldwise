5ea2d73ee3225e39b6de8e47191031bf5ee5e570
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-05-28 13:56:14 +0330

    fix: fix the back button by navige to true replase

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
 
   useEffect(
     function () {
-      if (isAuthenticated) navigate("/app");
+      if (isAuthenticated) navigate("/app", { replace: true });
     },
     [isAuthenticated, navigate]
   );
