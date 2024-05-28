ecc8c4557a61a5fab22141b9618fab06ff2070a9
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-05-28 13:33:01 +0330

    feat: add the handleSubmit function for form in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,16 +2,23 @@ import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
 import { useState } from "react";
 import Button from "../components/Button";
+import { useAuth } from "../contexts/FakeAuthContext";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login } = useAuth();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
