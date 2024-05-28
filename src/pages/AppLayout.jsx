aa1ca6bb35a96e0687bb1d89acf3938751013538
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-05-28 14:03:55 +0330

    feat: create the handle click for logout and navigate to the homepage

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,12 +1,14 @@
 import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map";
+import User from "../components/User";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
       <Sidebar />
       <Map />
+      <User />
     </div>
   );
 }
