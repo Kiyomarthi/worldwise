eb0ee22ba3c1cab1a66da8998e47a67d88239279
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-05-28 13:07:52 +0330

    feat: add the useReducer for user and Authenticate

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -1,9 +1,19 @@
+import { useReducer } from "react";
 import { useContext } from "react";
 import { createContext } from "react";
 
 const AuthContext = createContext();
+const initialState = {
+    user : null,
+    isAuthenticated: false;
+}
+
+function reducer(state, action){
+    switch(action.type)
+}
 
 function authProvider({ children }) {
+    const [{user, isAuthenticated}, dispatch] = useReducer(reducer, initialState)
     function login(user, password) {}
     function logout() {}
   return <AuthContext.Provider>{children}</AuthContext.Provider>;
