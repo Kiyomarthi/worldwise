e76526820dc6936116d7c235fccc16d81505c612
Kiyomarthi <a.kiyomarthi@gmail.com>
2024-05-28 13:18:29 +0330

    feat: complate the login and logout function and export the AouthContext and useAuth

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -7,6 +7,12 @@ const initialState = {
   user: null,
   isAuthenticated: false,
 };
+const FAKE_USER = {
+  name: "Jack",
+  email: "jack@example.com",
+  password: "qwerty",
+  avatar: "https://i.pravatar.cc/100?u=zz",
+};
 
 function reducer(state, action) {
   switch (action.type) {
@@ -25,8 +31,13 @@ function authProvider({ children }) {
     reducer,
     initialState
   );
-  function login(user, password) {}
-  function logout() {}
+  function login(email, password) {
+    if (email === FAKE_USER.email && password === FAKE_USER.password)
+      dispatch({ type: "login", payload: FAKE_USER });
+  }
+  function logout() {
+    dispatch({ type: "logout" });
+  }
   return (
     <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
       {children}
@@ -40,3 +51,5 @@ function useAuth() {
     throw new Error("authContext was used outside the authProvider");
   return context;
 }
+
+export { AuthContext, useAuth };
